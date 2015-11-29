1251ee4fef3a7e16e5024c9b0cbadf06e9d9576a
Oleg <mas@tanec.lv>
2015-11-29 22:42:46 +0200

    50% of 640 width

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
+
     $(".finder_date").datepicker({
         dateFormat: "M dd"
     });
+    $(".menu_button").click(function() {
+        $(".header_buttons").toggleClass("active");
+    });
+
 });
